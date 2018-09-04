6ec410ed91d333c4ea6204f8b77b1449e19a25b8
Ian Taylor <iantaylor1610@gmail.com>
2018-09-04 08:33:05 +0100

    Add project files.

---
+++ Assets/Scripts/ButtonBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviourScript : MonoBehaviour {
+ 
+	// Use this for initialization
+	public void OnButtonPressed(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
---
+++ Assets/Scripts/MoveToMainScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToMainScene : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
